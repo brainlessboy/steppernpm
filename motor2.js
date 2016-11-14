44926749b6213edc25da0a8853fb7e03fb5dafc2
Oliver Brupbacher <oliver.brupbacher@appway.com>
2016-11-14 22:42:44 +0100

    adapted test

--- motor2.js
+++ motor2.js
@@ -2,7 +2,10 @@ var stepper = require('pisteppermotor');
 
 try {
     stepper.init(0x60);
-    stepper.test();
+    var steps = 100;
+    var speed = 50;
+    var motor = 1;
+    stepper.moveForward(steps,speed,motor);
 } catch (err) {
     console.log(err);
 }
